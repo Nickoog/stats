624937814619e8f2d2a7022f0d6701bf6dc0e8c4
nicolas <nicolas.hoog@gmail.com>
2019-11-23 15:28:59 -0500

    generate HTML reports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-console.log(`Man United won ${manUnitedWins} games`);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+
+summary.buildAndPrintReport(matchReader.matches);
